24dcb61b008eaec4d70dd37f120299d995fc7db1
Paulo Vitor <pv.marques2010@hotmail.com>
2026-08-16 12:17:57 -0300

    feat: criando a camada service da agencia

---
+++ banking-service/src/main/java/br/com/alura/service/http/AgencyService.java
@@ -0,0 +1,45 @@
+package br.com.alura.service.http;
+
+import br.com.alura.domain.Agency;
+import br.com.alura.domain.http.AgencyHttp;
+import br.com.alura.domain.http.RegistrationStatus;
+import br.com.alura.exception.AgencyNotFoundException;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ApplicationScoped
+public class AgencyService {
+
+    @RestClient
+    private RegistrationStatusHttpService registrationStatusHttpService;
+
+    private List<Agency> agencies = new ArrayList<>();
+
+    public void register(Agency agency){
+        AgencyHttp agencyHttp = registrationStatusHttpService.searchCpnj(agency.getCnpj());
+
+        if(agencyHttp != null && agencyHttp.getRegistrationStatus().equals(RegistrationStatus.ACTIVE)){
+            agencies.add(agency);
+        } else {
+            throw new AgencyNotFoundException();
+        }
+    }
+
+    public Agency searchAgenctById(Integer id){
+        return agencies.stream().filter( agency -> agency.getId().equals(id)).toList().getFirst();
+    }
+
+    public void deleteAgencyById(Integer id){
+        agencies.removeIf(agency -> agency.getId().equals(id));
+    }
+
+    public void updateAgency(Agency agency){
+        deleteAgencyById(agency.getId());
+        register(agency);
+    }
+
+}
